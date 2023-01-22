42a3494a5f09387fd860f34f19347cebb8f85737
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2023-01-22 18:21:36 +0900

    Revert "Use toggleterm instead of floatterm"
    This reverts commit 457a8bfadcde4632a38c0f0868b965bb7e5649a1.

--- lua/plugins/plugin_setup.lua
+++ lua/plugins/plugin_setup.lua
@@ -30,7 +30,7 @@ return require("packer").startup(function(use)
     use "tpope/vim-surround"
     use "vim-scripts/ReplaceWithRegister"
     use "numToStr/Comment.nvim"
-    use { "akinsho/toggleterm.nvim", tag = "*" }
+    use "voldikss/vim-floaterm"
 
     use "nvim-tree/nvim-tree.lua"
     use "nvim-lualine/lualine.nvim"
--- lua/plugins/toggleterm.lua
+++
@@ -1,3 +0,0 @@
-require("toggleterm").setup()
-vim.keymap.set("n", "t", ":ToggleTerm size=20<CR>")
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>:q<CR>")
