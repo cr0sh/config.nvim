764f3e1983cc9c726ddd7f0946c9f1f9b0e55376
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2023-02-01 09:31:54 +0900

    Fix typescript formatting

--- lua/plugins/lsp/typescript.lua
+++ lua/plugins/lsp/typescript.lua
@@ -1,20 +1,22 @@
-lspconfig = require "lspconfig"
+lspconfig = require("lspconfig")
 
 local on_attach = function(client, bufnr)
     LSP_ON_ATTACH(client, bufnr)
     -- format on save
-    -- if client.server_capabilities.documentFormattingProvider then
-    --     vim.api.nvim_create_autocmd("BufWritePre", {
-    --         group = vim.api.nvim_create_augroup("Format", { clear = true }),
-    --         buffer = bufnr,
-    --         callback = function() vim.lsp.buf.format { async = true } end
-    --     })
-    -- end
+    if client.server_capabilities.documentFormattingProvider then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = vim.api.nvim_create_augroup("Format", { clear = true }),
+            buffer = bufnr,
+            callback = function()
+                vim.lsp.buf.format()
+            end,
+        })
+    end
 end
 
 -- TypeScript
-lspconfig["tsserver"].setup {
+lspconfig["tsserver"].setup({
     on_attach = on_attach,
     filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-    cmd = { "typescript-language-server", "--stdio" }
-}
+    cmd = { "typescript-language-server", "--stdio" },
+})
