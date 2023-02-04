4f8b01591c2234fb65c2e189ca21632390a6080c
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2023-02-04 14:51:56 +0900

    Remove lsp-saga

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -1,24 +1,5 @@
 local lspconfig = require("lspconfig")
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
-local saga = require("lspsaga")
-
-saga.setup({
-    border_style = "rounded",
-    -- keybinds for navigation in lspsaga window
-    move_in_saga = { prev = "<C-k>", next = "<C-j>" },
-    -- use enter to open file with finder
-    finder_action_keys = {
-        open = "<CR>",
-        scroll_down = "<C-f>",
-        scroll_up = "<C-b>", -- quit can be a table
-    },
-    -- use enter to open file with definition preview
-    definition_action_keys = {
-        edit = "<CR>",
-    },
-    preview_lines_above = 20,
-    max_preview_lines = 20,
-})
 
 -- enable keybinds only for when lsp server available
 LSP_ON_ATTACH = function(client, bufnr)
--- lua/plugins/plugin_setup.lua
+++ lua/plugins/plugin_setup.lua
@@ -23,100 +23,105 @@ if not status then
 end
 
 return require("packer").startup(function(use)
-    use "wbthomason/packer.nvim"
-    use "lewis6991/impatient.nvim"
-    use "rcarriga/nvim-notify"
-    use "timonv/vim-cargo"
-    use "tpope/vim-surround"
-    use "vim-scripts/ReplaceWithRegister"
-    use "numToStr/Comment.nvim"
-    use "voldikss/vim-floaterm"
+    use("wbthomason/packer.nvim")
+    use("lewis6991/impatient.nvim")
+    use("rcarriga/nvim-notify")
+    use("timonv/vim-cargo")
+    use("tpope/vim-surround")
+    use("vim-scripts/ReplaceWithRegister")
+    use("numToStr/Comment.nvim")
+    use("voldikss/vim-floaterm")
 
-    use "nvim-tree/nvim-tree.lua"
-    use "nvim-lualine/lualine.nvim"
-    use "akinsho/bufferline.nvim"
+    use("nvim-tree/nvim-tree.lua")
+    use("nvim-lualine/lualine.nvim")
+    use("akinsho/bufferline.nvim")
 
-    use "rmagatti/auto-session"
+    use("rmagatti/auto-session")
 
-    use "jose-elias-alvarez/null-ls.nvim"
-    use "williamboman/mason.nvim"
-    use "williamboman/mason-lspconfig.nvim"
-    use "simrat39/rust-tools.nvim"
-    use "jayp0521/mason-null-ls.nvim"
-    use "glepnir/lspsaga.nvim"
-    use {
-        'saecki/crates.nvim',
-        tag = 'v0.3.0',
-        requires = { 'nvim-lua/plenary.nvim' },
+    use("jose-elias-alvarez/null-ls.nvim")
+    use("williamboman/mason.nvim")
+    use("williamboman/mason-lspconfig.nvim")
+    use("simrat39/rust-tools.nvim")
+    use("jayp0521/mason-null-ls.nvim")
+    use({
+        "saecki/crates.nvim",
+        tag = "v0.3.0",
+        requires = { "nvim-lua/plenary.nvim" },
         config = function()
-            require('crates').setup()
+            require("crates").setup()
         end,
-    }
+    })
 
-    use "ziglang/zig.vim"
+    use("ziglang/zig.vim")
 
-    use "nvim-lua/plenary.nvim"
-    use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" } -- dependency for better sorting performance
-    use { "nvim-telescope/telescope.nvim", branch = "0.1.x" } -- fuzzy finder
-    use {
+    use("nvim-lua/plenary.nvim")
+    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+    use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+    use({
         "nvim-telescope/telescope-frecency.nvim",
         config = function()
-            require "telescope".load_extension("frecency")
+            require("telescope").load_extension("frecency")
         end,
-        requires = { "kkharji/sqlite.lua" }
-    }
+        requires = { "kkharji/sqlite.lua" },
+    })
 
     -- use "tanvirtin/monokai.nvim"
-    use "navarasu/onedark.nvim"
+    use("navarasu/onedark.nvim")
 
     -- configuring lsp servers
-    use "jose-elias-alvarez/null-ls.nvim"
+    use("jose-elias-alvarez/null-ls.nvim")
 
-    use "neovim/nvim-lspconfig" -- easily configure language servers
-    use "ray-x/lsp_signature.nvim"
+    use("neovim/nvim-lspconfig") -- easily configure language servers
+    use("ray-x/lsp_signature.nvim")
 
-    use "hrsh7th/nvim-cmp"
-    use "hrsh7th/cmp-nvim-lsp" -- for autocompletion
+    use("hrsh7th/nvim-cmp")
+    use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
     -- Useful completion sources:
-    use 'hrsh7th/cmp-nvim-lua'
-    use 'hrsh7th/cmp-nvim-lsp-signature-help'
-    use 'hrsh7th/cmp-vsnip'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/vim-vsnip'
-    use "jose-elias-alvarez/typescript.nvim" -- additional functionality for typescript server (e.g. rename file & update imports
-    use "onsails/lspkind.nvim" -- vs-code like icons for autocompletion
-    use "L3MON4D3/LuaSnip"
-    use "windwp/nvim-autopairs"
-    use "p00f/nvim-ts-rainbow"
+    use("hrsh7th/cmp-nvim-lua")
+    use("hrsh7th/cmp-nvim-lsp-signature-help")
+    use("hrsh7th/cmp-vsnip")
+    use("hrsh7th/cmp-path")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/vim-vsnip")
+    use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports
+    use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+    use("L3MON4D3/LuaSnip")
+    use("windwp/nvim-autopairs")
+    use("p00f/nvim-ts-rainbow")
 
     -- treesitter configuration
-    use {
+    use({
         "nvim-treesitter/nvim-treesitter",
         run = function()
             require("nvim-treesitter.install").update({ with_sync = true })
         end,
-    }
-    use {
+    })
+    use({
         "folke/trouble.nvim",
         requires = "kyazdani42/nvim-web-devicons",
-    }
+    })
 
-    use "TimUntersberger/neogit"
+    use("TimUntersberger/neogit")
 
-    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install",
-        setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
-    use {
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = "cd app && npm install",
+        setup = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    })
+    use({
         "folke/todo-comments.nvim",
         requires = "nvim-lua/plenary.nvim",
         config = function()
-            require("todo-comments").setup {
+            require("todo-comments").setup({
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
                 -- refer to the configuration section below
-            }
-        end
-    }
+            })
+        end,
+    })
 
     if packer_bootstrap then
         require("packer").sync()
