44a7fc441144a4595d0da50150d4d5398b1590ca
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2023-02-24 23:14:18 +0900

    Fix typescript format-on-save conflict with null-ls

--- lua/plugins/lsp/typescript.lua
+++ lua/plugins/lsp/typescript.lua
@@ -2,16 +2,6 @@ lspconfig = require("lspconfig")
 
 local on_attach = function(client, bufnr)
     LSP_ON_ATTACH(client, bufnr)
-    -- format on save
-    if client.server_capabilities.documentFormattingProvider then
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = vim.api.nvim_create_augroup("Format", { clear = true }),
-            buffer = bufnr,
-            callback = function()
-                vim.lsp.buf.format()
-            end,
-        })
-    end
 end
 
 -- TypeScript
