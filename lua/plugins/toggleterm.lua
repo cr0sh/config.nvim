13a83f7fad3e55bb1fd84b45283ce3f6844c8c04
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2023-03-31 13:40:08 +0900

    Fix incorrect toggleterm keymap

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,4 +3,3 @@ vim.api.nvim_create_autocmd(
     { pattern = "term://*toggleterm#*", command = [[tnoremap <silent><Esc> <Cmd>exe v:count1 . "ToggleTerm"<CR>]] }
 )
 vim.keymap.set("n", "t", [[<Cmd>exe v:count1 . "ToggleTerm"<CR>]], { silent = true })
-vim.keymap.set("i", "<Esc>", [[<Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>]], { silent = true })
