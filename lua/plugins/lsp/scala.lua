551ce01aeba7e1189bc998562ba913239f7e370a
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2023-04-02 23:27:59 +0900

    Forcefully apply save-on-format setting on Scala files

--- lua/plugins/lsp/scala.lua
+++ lua/plugins/lsp/scala.lua
@@ -1,1 +1,13 @@
-require("lspconfig").metals.setup({ on_attach = LSP_ON_ATTACH })
+require("lspconfig").metals.setup({
+    on_attach = function(client, bufnr)
+        LSP_ON_ATTACH(client, bufnr)
+
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                vim.lsp.buf.format({ bufnr = bufnr, timeout_ms = 2000 })
+            end,
+        })
+    end,
+})
