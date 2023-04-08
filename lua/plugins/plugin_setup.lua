0543c2c34883adaf9174e55ae4a84a9a62cd9edc
Junghyun Nam <13118114+cr0sh@users.noreply.github.com>
2023-04-08 21:31:22 +0900

    Remove colorscheme

--- lua/plugins/plugin_setup.lua
+++ lua/plugins/plugin_setup.lua
@@ -61,14 +61,6 @@ return require("packer").startup(function(use)
         requires = { "kkharji/sqlite.lua" },
     })
 
-    -- use("navarasu/onedark.nvim")
-    use({
-        "shaunsingh/solarized.nvim",
-        config = function()
-            require("solarized").set()
-        end,
-    })
-
     use("neovim/nvim-lspconfig") -- easily configure language servers
     use("ray-x/lsp_signature.nvim")
 
