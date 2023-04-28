d7ae93c3eeacde95f7d091efff35a45d547566a0
Junghyun Nam <13118114+cr0sh@users.noreply.github.com>
2023-04-28 23:32:28 +0900

    go: Use goimports to reorganize imports

--- lua/plugins/lsp/go.lua
+++ lua/plugins/lsp/go.lua
@@ -1,13 +1,5 @@
 require("lspconfig").gopls.setup({
-    on_attach = function(client, bufnr)
-        LSP_ON_ATTACH(client, bufnr)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*.go",
-            callback = function()
-                vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
-            end,
-        })
-    end,
+    on_attach = LSP_ON_ATTACH,
     settings = {
         gopls = {
             analyses = {
