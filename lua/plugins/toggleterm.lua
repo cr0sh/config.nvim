3b1298ededaab3193826fe0936bddd6320e5d970
Junghyun Nam <13118114+cr0sh@users.noreply.github.com>
2023-05-02 20:10:10 +0900

    Close trouble window when opening toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_autocmd(
     "TermEnter",
     { pattern = "term://*toggleterm#*", command = [[tnoremap <silent><Esc> <Cmd>exe v:count1 . "ToggleTerm"<CR>]] }
 )
-vim.keymap.set("n", "t", [[<Cmd>exe v:count1 . "ToggleTerm"<CR>]], { silent = true })
+vim.keymap.set("n", "t", [[<Cmd>TroubleClose<CR><Cmd>exe v:count1 . "ToggleTerm"<CR>]], { silent = true })
 
 vim.api.nvim_create_autocmd("VimLeavePre", {
     callback = function(_)
