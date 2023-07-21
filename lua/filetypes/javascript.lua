7e5beb0d3d41a5b12e07d3cc647e254ced1ff3ed
Junghyun Nam <13118114+cr0sh@users.noreply.github.com>
2023-07-21 22:40:13 +0900

    Set sw=2 for filetype=json,jsonc

--- lua/filetypes/javascript.lua
+++ lua/filetypes/javascript.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
     group = vim.api.nvim_create_augroup("javascript_sw_is_2", { clear = true }),
-    pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+    pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact", "json", "jsonc" },
     command = "setlocal shiftwidth=2",
 })
