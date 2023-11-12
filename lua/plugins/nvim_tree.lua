0625b8963ef20af7c115e5bca608cd68d0aa4bca
Junghyun Nam <13118114+cr0sh@users.noreply.github.com>
2023-11-12 22:40:31 +0900

    Fix auto-session problem

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -10,16 +10,15 @@ nvimtree.setup({
     hijack_netrw = true,
 })
 
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-    pattern = "NvimTree*",
-    callback = function()
-        local view = require("nvim-tree.view")
-        local is_visible = view.is_visible()
-
-        local api = require("nvim-tree.api")
-        if not is_visible then
-            api.tree.open()
-        end
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "NvimTree" },
+    callback = function(args)
+        vim.api.nvim_create_autocmd("VimLeavePre", {
+            callback = function()
+                vim.api.nvim_buf_delete(args.buf, { force = true })
+                return true
+            end,
+        })
     end,
 })
 
