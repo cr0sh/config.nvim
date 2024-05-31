cc412de1323c1e5437961f0d75fb1b6d71310c2d
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2024-05-31 10:24:02 +0900

    fix: trouble v3 breakage

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,6 +1,6 @@
 require("trouble").setup({ auto_preview = false })
 
-vim.keymap.set("n", "<leader>tt", "<cmd>TroubleToggle<CR>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>tt", "<cmd>Trouble diagnostics toggle<CR>", { silent = true, noremap = true })
 
 -- Trouble seems to have trouble with auto-session, so close when VimLeavePre
-vim.api.nvim_create_autocmd("VimLeavePre", { command = [[TroubleClose]] })
+vim.api.nvim_create_autocmd("VimLeavePre", { command = [[Trouble diagnostics close]] })
