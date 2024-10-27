2a3a3fea6cbb25bdff8f0403d70802d9dd3c2724
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2024-10-27 16:18:49 +0900

    Remove buggy toggleterm autoclose

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,14 +5,3 @@ vim.api.nvim_create_autocmd(
     { pattern = "term://*toggleterm#*", command = [[tnoremap <silent><Esc> <Cmd>exe v:count1 . "ToggleTerm"<CR>]] }
 )
 vim.keymap.set("n", "t", [[<Cmd>Trouble diagnostics close<CR><Cmd>exe v:count1 . "ToggleTerm"<CR>]], { silent = true })
-
-vim.api.nvim_create_autocmd("VimLeavePre", {
-    callback = function(_)
-        local get_ls = vim.tbl_filter(function(buf)
-            return vim.api.nvim_buf_is_valid(buf) and string.find(vim.api.nvim_buf_get_name(buf), "toggleterm#") ~= nil
-        end, vim.api.nvim_list_bufs())
-        for _, buf in pairs(get_ls) do
-            vim.api.nvim_buf_delete(buf, { force = true })
-        end
-    end,
-})
