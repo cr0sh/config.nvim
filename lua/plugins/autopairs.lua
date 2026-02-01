0235f3ff51091df4421eefdb82379f81e8f8e22e
Junghyun Nam <13118114+cr0sh@users.noreply.github.com>
2026-02-02 00:27:35 +0900

    Autopairs problem looks like resolved?

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -2,5 +2,3 @@ require("nvim-autopairs").setup()
 
 -- https://github.com/windwp/nvim-autopairs/issues/90#issuecomment-889282516
 vim.opt.viewoptions = { "folds", "cursor" }
--- Also remove "localoptions" from auto-session options
-vim.opt.sessionoptions = { "blank", "buffers", "curdir", "folds", "help", "tabpages", "winsize", "winpos", "terminal" }
