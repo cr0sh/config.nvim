c2d03889cd30f87c5107e2d936d58ef220a5ebc3
Junghyun Nam <13118114+cr0sh@users.noreply.github.com>
2026-02-04 16:50:43 +0900

    Update rustaceanvim config

--- lua/plugins/lsp/rust.lua
+++ lua/plugins/lsp/rust.lua
@@ -1,13 +1,6 @@
-vim.lsp.config("rustaceanvim", {
-    on_attach = LSP_ON_ATTACH,
-    settings = {
-        -- to enable rust-analyzer settings visit:
-        -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-        ["rust-analyzer"] = {
-            -- enable clippy on save
-            check = {
-                command = "clippy",
-            },
-        },
+vim.g.rustaceanvim = {
+    server = {
+        on_attach = LSP_ON_ATTACH,
+        enable_clippy = true,
     },
-})
+}
