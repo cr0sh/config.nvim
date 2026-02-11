f1fea998801327b5e09ad0ffa164378cef7a3153
Junghyun Nam <13118114+cr0sh@users.noreply.github.com>
2026-02-11 11:58:22 +0900

    Also set Pmenu bg none

--- lua/plugins/lazy_setup.lua
+++ lua/plugins/lazy_setup.lua
@@ -194,10 +194,12 @@ require("lazy").setup({
             })
             -- Fix broken bg(why?)
             vim.api.nvim_set_hl(0, "SnacksPicker", { bg = "none", nocombine = true })
+            vim.api.nvim_set_hl(0, "Pmenu", { bg = "none", nocombine = true })
             vim.api.nvim_create_autocmd("ColorScheme", {
                 pattern = "*",
                 callback = function()
                     vim.api.nvim_set_hl(0, "SnacksPicker", { bg = "none", nocombine = true })
+                    vim.api.nvim_set_hl(0, "Pmenu", { bg = "none", nocombine = true })
                 end,
             })
         end,
