78615056ee72fb9d0e6af2f9899c7918eb288189
Junghyun Nam <13118114+cr0sh@users.noreply.github.com>
2026-02-14 10:42:28 +0900

    Tidy things

--- lua/plugins/lazy_setup.lua
+++ lua/plugins/lazy_setup.lua
@@ -140,6 +140,37 @@ require("lazy").setup({
     { "L3MON4D3/LuaSnip" },
     { "windwp/nvim-autopairs" },
     { "hiphish/rainbow-delimiters.nvim" },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        init = function()
+            require("noice").setup({
+                lsp = {
+                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                    },
+                },
+                -- you can enable a preset for easier configuration
+                presets = {
+                    bottom_search = true, -- use a classic bottom cmdline for search
+                    command_palette = true, -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = false, -- add a border to hover docs and signature help
+                },
+            })
+        end,
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+        },
+    },
 
     -- Treesitter
     {
@@ -217,7 +248,7 @@ require("lazy").setup({
             explorer = { enabled = true },
             indent = { enabled = true },
             input = { enabled = true },
-            notifier = { enabled = true },
+            notifier = { enabled = true, gap = nil, margin = { top = 4, right = 3, bottom = 0 } },
             terminal = {
                 enabled = true,
                 win = {
